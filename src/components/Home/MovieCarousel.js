a7a6b3439d7276caeef7058c6e0fbde52749a5a6
davidhavlin <davidhavlin@gmail.com>
2020-11-29 22:19:31 +0100

    male upravy

--- src/components/Home/MovieCarousel.js
+++ src/components/Home/MovieCarousel.js
@@ -12,6 +12,7 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 	const [firstTime, setFirstTime] = useState(true);
 	const [counter, setCounter] = useState(0);
 	const [reRender, setReRender] = useState(false);
+	const [teleporting, setTeleporting] = useState(false);
 
 	const [transitioning, setTransitioning] = useState(false);
 	useEffect(() => {
@@ -26,7 +27,10 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 	const [itemsVisible, setItemsVisible] = useState(5);
 	const [width_of_item, set_width_of_item] = useState(20);
 	useEffect(() => {
-		if (window.innerWidth > 1000) {
+		if (window.innerWidth > 1400) {
+			setItemsVisible(6);
+			set_width_of_item(17);
+		} else if (window.innerWidth < 1000) {
 			setItemsVisible(5);
 			set_width_of_item(20);
 		} else if (window.innerWidth < 600) {
@@ -42,10 +46,10 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 		window.addEventListener("resize", debounce(handleResizeEvent, 300), {
 			once: true,
 		});
-	}, [counter, firstTime, reRender]);
+	}, [counter, firstTime]);
 
 	const handleResizeEvent = () => {
-		console.log("resize wtf", counter);
+		console.log("resize wtf", itemsVisible);
 		// 	if (window.innerWidth > 1000) {
 		// 		setItemsVisible(5);
 		// 		set_width_of_item(20);
@@ -65,7 +69,9 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 		// 			-100 * (counter + 1) - width_of_item
 		// 		}%)`;
 		// 	}
-		if (window.innerWidth > 1000) {
+		if (window.innerWidth > 1400) {
+			doSomething(6, 16.7);
+		} else if (window.innerWidth < 1000) {
 			doSomething(5, 20);
 		} else if (window.innerWidth < 600) {
 			doSomething(2, 50);
@@ -77,31 +83,62 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 	};
 	const doSomething = (items, itemWidth) => {
 		// carousel.current.style.transform = `translateX(${-100 - itemWidth}%)`;
-		// if (counter === 0 && firstTime) {
-		// 	console.log();
-		// 	carousel.current.style.transform = `translateX(${0}%)`;
-		// } else {
-		// 	console.log("toto nie?");
-		// 	carousel.current.style.transform = `translateX(${
-		// 		-100 * (counter + 1) - width_of_item
-		// 	}%)`;
-		// }
-		// setReRender(true);
+		if (counter === 0 && firstTime) {
+			carousel.current.style.transform = `translateX(${0}%)`;
+		} else {
+			carousel.current.style.transform = `translateX(${
+				-100 * (counter + 1) - width_of_item
+			}%)`;
+		}
+		setReRender(true);
 		setItemsVisible(items);
 		set_width_of_item(itemWidth);
 		movies.length = 20;
-		// setReRender(false);
+		setReRender(false);
 	};
 	useEffect(() => {
-		console.log(itemsVisible);
+		// console.log("proste nechapem", itemsVisible);
 	}, [itemsVisible]);
 
 	useEffect(() => {
 		const div = carousel.current;
+		// console.log(itemsVisible);
 		// prettier-ignore
-		firstTime 
-            ? div.style.transform = `translateX(${ -100 * counter }%)`
-            : div.style.transform = `translateX(${ -100 * (counter + 1) - width_of_item }%)`;
+		if (teleporting) return;
+		if (movies.length % itemsVisible === 0) {
+			firstTime
+				? (div.style.transform = `translateX(${-100 * counter}%)`)
+				: (div.style.transform = `translateX(${
+						-100 * (counter + 1) - width_of_item
+				  }%)`);
+		} else {
+			let lastItem = Math.floor(movies.length / itemsVisible);
+
+			if (firstTime) {
+				div.style.transform = `translateX(${-100 * counter}%)`;
+			} else {
+				console.log("DOPICE");
+				if (counter === lastItem) {
+					div.style.transform = `translateX(${
+						-100 * (counter + 1) + 1
+					}%)`;
+				} else if (counter === lastItem + 1) {
+					div.style.transform = `translateX(${
+						-100 * (counter + 1) + 1
+					}%)`;
+				} else {
+					div.style.transform = `translateX(${
+						-100 * (counter + 1) - width_of_item
+					}%)`;
+				}
+			}
+
+			// firstTime
+			// ? (div.style.transform = `translateX(${-100 * counter}%)`)
+			// : (div.style.transform = `translateX(${
+			// 		-100 * (counter + 1) - width_of_item
+			//   }%)`);
+		}
 
 		if (counter === -1) {
 			setRealCount(Math.floor(movies.length / itemsVisible - 1));
@@ -134,7 +171,7 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 			parent.style.transition = "none";
 			parent.style.transform = `translateX(${-100 * 2 - width_of_item}%)`;
 			setTimeout(() => {
-				parent.style.transition = "transform 1s ease";
+				parent.style.transition = "transform 0.6s ease";
 			}, 0);
 		} else {
 			return;
@@ -146,34 +183,56 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 		let lastItem = Math.floor(movies.length / itemsVisible);
 		setTransitioning(false);
 		carousel.current.style.pointerEvents = "initial";
-
-		if (counter === 1) {
-			firstSlide();
-		} else if (counter === lastItem) {
-			teleportTo("start");
-		} else if (counter === -1) {
-			teleportTo("end");
+		if (movies.length % itemsVisible === 0) {
+			if (counter === 1) {
+				firstSlide();
+			} else if (counter === lastItem) {
+				teleportTo("start");
+			} else if (counter === -1) {
+				teleportTo("end");
+			}
+		} else {
+			if (counter === 1) {
+				firstSlide();
+			} else if (counter === lastItem + 1) {
+				teleportTo("start");
+			} else if (counter === -1) {
+				teleportTo("endOdd");
+			}
 		}
 	};
 
 	const teleportTo = (where) => {
+		setTeleporting(true);
+		console.log(where);
 		const parent = carousel.current;
+		// prettier-ignore
 		if (where === "start") {
-			setCounter(0);
+            let c = 0; 
 			parent.style.transition = "none";
 			parent.style.transform = `translateX(${
-				-100 * (counter + 1) - width_of_item
-			}%)`;
-		} else {
+				-100 * (c + 1) - width_of_item
+            }%)`;
+            setCounter(c);
+		} else if (where === "end") {
+            let c = Math.floor(movies.length / itemsVisible - 1);
 			parent.style.transition = "none";
 			parent.style.transform = `translateX(${
-				-100 * (counter + 1) - width_of_item
+				-100 * (c + 1) - width_of_item
 			}%)`;
-			setCounter(Math.floor(movies.length / itemsVisible - 1));
-			// console.log(movies.length / itemsVisible - 1);
+			setCounter(c);
+		} else if (where === "endOdd") {
+            let c = Math.floor(movies.length / itemsVisible - 1);
+            parent.style.transition = "none";
+            // parent.style.transform = `translateX(${-100 * (c + 1) }%)`;
+            parent.style.transform = `translateX(${-100 * (c + 1) - (width_of_item*3)}%)`;
+            
+			console.log(-100 * (c + 1) - (width_of_item*3)); // musi byt 700
+            setCounter(c);
 		}
 		setTimeout(() => {
-			parent.style.transition = "transform 1s ease";
+			parent.style.transition = "transform 0.6s ease";
+			setTeleporting(false);
 		}, 50);
 	};
 
@@ -208,6 +267,11 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 		console.log(e.target.id);
 	};
 
+	const makeItEven = (i) => {
+		// pokial sa neda cislo delit poctom filmov tak upravim pocet filmov
+		return movies.length % itemsVisible !== 0 ? i !== 18 && i !== 19 : true;
+	};
+
 	return (
 		<section className={`movies-section ${big ? "" : "small-version"}`}>
 			<h3 className="section-title">{title}</h3>
@@ -224,70 +288,85 @@ const MovieCarousel = ({ title, movies, setMovies, big }) => {
 				onTransitionEnd={handleTransitionEnd}
 			>
 				{movies &&
-					movies.map((movie, index) => (
-						<div className={firstOrLastItem(index)} key={movie.id}>
-							<div
-								className="movie"
-								style={{
-									backgroundImage: `url(${
-										imgurl + movie.poster_path
-									})`,
-								}}
-							>
+					movies.map((movie, index) => {
+						if (makeItEven(index)) {
+							return (
 								<div
-									className="hovered-show"
-									style={{
-										backgroundImage: `url(${
-											imgurl + movie.backdrop_path
-										})`,
-									}}
+									className={firstOrLastItem(index)}
+									key={movie.id}
 								>
-									<div className="content-hovered">
-										<div className="content">
-											<section className="buttons-section">
-												<div>
-													<button className="btn-play">
-														<i className="fas fa-play"></i>
-													</button>
-													<button
-														className="btn-add"
-														id={movie.id}
-														onClick={addToFavorite}
-													>
-														<span className="icon-tooltip">
-															Add to My List
-														</span>
-														<i className="fas fa-plus"></i>
-													</button>
-												</div>
-												<button className="btn-info">
-													<span className="icon-tooltip">
-														More Info
-													</span>
+									<div
+										className="movie"
+										style={{
+											backgroundImage: `url(${
+												imgurl + movie.poster_path
+											})`,
+										}}
+									>
+										<div
+											className="hovered-show"
+											style={{
+												backgroundImage: `url(${
+													imgurl + movie.backdrop_path
+												})`,
+											}}
+										>
+											<div className="content-hovered">
+												<div className="content">
+													<section className="buttons-section">
+														<div>
+															<button className="btn-play">
+																<i className="fas fa-play"></i>
+															</button>
+															<button
+																className="btn-add"
+																id={movie.id}
+																onClick={
+																	addToFavorite
+																}
+															>
+																<span className="icon-tooltip">
+																	Add to My
+																	List
+																</span>
+																<i className="fas fa-plus"></i>
+															</button>
+														</div>
+														<button className="btn-info">
+															<span className="icon-tooltip">
+																More Info
+															</span>
 
-													<i className="fas fa-chevron-down"></i>
-												</button>
-											</section>
-											<section className="text-section">
-												<h4>
-													{movie.name || movie.title}
-												</h4>
-												<p>
-													Rating: {movie.vote_average}
-													<span className="age-res">
-														{movie.vote_average > 8
-															? "16+"
-															: "12+"}
-													</span>{" "}
-													<span>Limited Series</span>
-												</p>
-											</section>
+															<i className="fas fa-chevron-down"></i>
+														</button>
+													</section>
+													<section className="text-section">
+														<h4>
+															{movie.name ||
+																movie.title}
+														</h4>
+														<p>
+															Rating:{" "}
+															{movie.vote_average}
+															<span className="age-res">
+																{movie.vote_average >
+																8
+																	? "16+"
+																	: "12+"}
+															</span>{" "}
+															<span>
+																Limited Series
+															</span>
+														</p>
+													</section>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-					))}
+							);
+						}
+					})}
 			</div>
 
 			{clicked && (
